5ef10bfafc049bfaa314e0dd8ef524e70e12289a
Solomon <musolomoni@gmail.com>
2022-09-02 04:59:43 +0300

    Underline

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,13 @@
 function Footer() {
   return (
     <div className="flex p-2 justify-center">
-      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin ">
+      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin hover:text-decoration-underline ">
         Contact me
       </p>
-      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin ">
+      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin hover:text-decoration-underline">
         About
       </p>
-      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin ">
+      <p className="text-xs mx-2 text-zinc-500 hover:text-zinc-600 hover:cursor-pointer hover:font-bold font-josefin hover:text-decoration-underline">
         @soFton
       </p>
     </div>
